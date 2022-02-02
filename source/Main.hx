e2e1c2583dadbac61034a48d5e440d360be1bbde
Sabine <35895538+sayofthelor@users.noreply.github.com>
2022-02-02 16:26:28 -0500

    undo thingy???????

--- source/Main.hx
+++ source/Main.hx
@@ -10,7 +10,6 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
-import lore.Framerate;
 
 class Main extends Sprite
 {
@@ -21,7 +20,7 @@ class Main extends Sprite
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	public static var fpsVar:lore.Framerate;
+	public static var fpsVar:FPS;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
