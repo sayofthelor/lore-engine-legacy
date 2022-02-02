cb8d46e07792286a85249f40d0e56d47750834b3
Sabine <35895538+sayofthelor@users.noreply.github.com>
2022-02-02 16:19:54 -0500

    fuck you *commits untested code*

--- source/Main.hx
+++ source/Main.hx
@@ -10,6 +10,7 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
+import lore.Framerate;
 
 class Main extends Sprite
 {
@@ -20,7 +21,7 @@ class Main extends Sprite
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	public static var fpsVar:FPS;
+	public static var fpsVar:lore.Framerate;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
