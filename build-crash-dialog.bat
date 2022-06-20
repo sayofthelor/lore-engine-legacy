d7407cfb3c2709a87385a47be557f082ef05b9e4
sayofthelor <35895538+sayofthelor@users.noreply.github.com>
2022-06-19 21:39:26 -0700

    clear branch

--- build-crash-dialog.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd crash-dialog
-echo Building crash dialog...
-lime build windows
-copy build\openfl\windows\bin\CrashDialog.exe ..\export\release\windows\bin\CrashDialog.exe
-cd ..
-
-@echo on
--- build-crash-dialog.sh
+++
@@ -1,5 +0,0 @@
-cd crash-dialog
-echo "Building crash dialog..."
-lime build linux
-cp build/openfl/linux/bin/CrashDialog ../export/release/linux/bin/CrashDialog
-cd ..
