624a6caeb3f4d8939c05f7ad413b6b8acca2c18b
Sailor Priddy <35895538+sayofthelor@users.noreply.github.com>
2022-12-17 22:25:04 -0500

    Update MacroTools.hx

--- source/lore/macros/MacroTools.hx
+++ source/lore/macros/MacroTools.hx
@@ -11,15 +11,12 @@ class MacroTools {
     // https://code.haxe.org/category/macros/add-git-commit-hash-in-build.html
     public static macro function getGitCommitHash():ExprOf<String> {
         #if (!display)
+        var commitHash:String = "";
         var process = new sys.io.Process('git', ['rev-parse', 'HEAD']);
         if (process.exitCode() != 0) {
           var message = process.stderr.readAll().toString();
-          var pos = haxe.macro.Context.currentPos();
-          haxe.macro.Context.error("Cannot execute `git rev-parse HEAD`. " + message, pos);
-        }
-        
-        // read the output of the process
-        var commitHash:String = process.stdout.readLine();
+          trace('git commit error: ${message}');
+        } else commitHash = process.stdout.readLine();
         
         // Generates a string expression
         return macro $v{commitHash};
